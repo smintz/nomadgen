53d29714bfb19778fcf0c0dbbe650022695c98f8
Shahar Mintz <smintz@packet.net>
2018-09-15 10:12:47 +0000

    Refactor util.py to use click for cli and NomadgenAPI for Nomad RPC

--- nomadgen/util.py
+++ nomadgen/util.py
@@ -1,79 +1,95 @@
-from thrift.transport.TTransport import TMemoryBuffer
-from thrift.protocol.TJSONProtocol import TSimpleJSONProtocol
-from thrift.Thrift import TType
-from jobspec.ttypes import NomadJob
-
-import requests
 import json
-import argparse
-class NomadJSONProtocol(TSimpleJSONProtocol):
-    FIELD_NAMES_TRANSLATIONS={
-        # "args" is a reseved word in thrift and therefore cannot be used as a field name.
-        "command_args": "args",
-    }
+import click
+import os
+from time import sleep
 
-    def writeFieldBegin(self, name, ttype, fid):
-        name = self.FIELD_NAMES_TRANSLATIONS.get(name, name)
-        self.writeJSONString(name)
+from .client import NomadgenAPI
+from .helpers import validate_json_output, jobToJSON, fromJson
 
-    def writeBool(self, boolean):
-        # This will make writeBool writing a json compatible boolean instead of 1/0
-        s="false"
-        if boolean:
-            s="true"
-        self.writeJSONNumber(s)
+nomadgen_client=NomadgenAPI()
 
-def writeToJSON(obj):
-  trans = TMemoryBuffer()
-  proto = NomadJSONProtocol(trans)
-  obj.write(proto)
-  return trans.getvalue()
+def abort_if_false(ctx, param, value):
+    if not value:
+        ctx.abort()
+@click.group()
+@click.option('--addr', default='http://127.0.0.1:4646', help='Address of nomad server')
+@click.option('--region', help='Run in region')
+@click.option('--skip-verify', is_flag=True, default=True, help='Skip TLS verification')
+@click.option('--cacert', type=click.Path(), help='Path of CA file')
+@click.option('--client-cert', type=click.Path())
+@click.option('--client-key', type=click.Path())
+def cli(addr, region, skip_verify, cacert, client_cert, client_key):
+    nomadgen_client.addr=addr
 
-def jobToJSON(job):
-    return writeToJSON(job)
+    if region:
+        nomadgen_client.set_region(region)
 
-def validate_json_output(text):
-    try:
-        j=json.loads(text)
-        print(json.dumps(j, indent=2))
-        return True
-    except:
-        print(text)
-        return False
+    if cacert:
+        nomadgen_client.set_ca(cacert)
+    else:
+        nomadgen_client.set_ca(not skip_verify)
 
-def cmdline():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-r', '--run', action='store_true', help='Will make nomad agent run the output')
-    parser.add_argument('-x', '--skip_diff', action='store_true', help='Will skip plan step')
-    parser.add_argument('-f', '--force', action='store_true', help='Should not wait for confirmation')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Run verbose logging')
-    parser.add_argument('--nomad_address', default='http://127.0.0.1:4646', help='Override default nomad agent address')
-    return parser.parse_args()
+    nomadgen_client.set_tls_key(client_key)
+    nomadgen_client.set_tls_cert(client_cert)
+    pass
+
+@click.command()
+@click.option('-j', '--json', is_flag=True, help='Show as json')
+def show(json):
+    job=nomadgen_client.job
+    if json:
+        validate_json_output(jobToJSON(job))
+    else:
+        click.echo(job)
+
+@click.command()
+def diff():
+    nomadgen_client.diff()
+
+@click.command()
+@click.option('-f', '--force', is_flag=True, help='Just run.')
+@click.option('-w', '--wait', is_flag=True, help='Wait for the deploy to complete.')
+@click.option('-p', '--promote', is_flag=True, help='Automatically promote the job if all canaries are healthy')
+def run(force, wait, promote):
+    nomadgen_client.diff()
+    if force:
+        cont = True
+    else:
+        cont = click.confirm("Continue?")
+    if cont:
+        d=nomadgen_client.run()
+
+    if wait:
+        nomadgen_client.wait(promote=promote)
+
+
+@click.command()
+def eval():
+    nomadgen_client.eval()
+
+@click.command()
+@click.confirmation_option(prompt="Are you sure you want to stop?")
+def stop():
+        nomadgen_client.stop()
+
+@click.command()
+@click.confirmation_option(prompt="Are you sure you want to restart?")
+def restart():
+        nomadgen_client.stop()
+        nomadgen_client.run()
+
+@click.command()
+def tf():
+    print(json.dumps({'output': jobToJSON(nomadgen_client.job)}))
+
+cli.add_command(show)
+cli.add_command(diff)
+cli.add_command(run)
+cli.add_command(stop)
+cli.add_command(restart)
+cli.add_command(eval)
+cli.add_command(tf)
 
 def export_if_last(job):
-    args=cmdline()
-    if args.verbose:
-        print(args)
-
-    if args.run:
-        if not args.skip_diff:
-            # Show the diff
-            result=requests.post(
-                args.nomad_address + "/v1/job/" + job.ID + "/plan",
-                data=jobToJSON(NomadJob(Job=job, Diff=True))
-            )
-
-            if not (validate_json_output(result.text)):
-                return
-
-        if args.force:
-            cont = "y"
-        else:
-            cont = raw_input("Continue?")
-        if cont in ["y", "Y"]:
-            payload=jobToJSON(NomadJob(Job=job))
-            result=requests.post(args.nomad_address + "/v1/job/" + job.ID, data=payload)
-            validate_json_output(result.text)
-    else:
-        validate_json_output(jobToJSON(job))
-
+    nomadgen_client.set_job(job)
+    cli(auto_envvar_prefix='NOMAD')
