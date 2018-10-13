300eab9e34a73632086b30314113b6acb6662260
Shahar Mintz <smintz@packet.net>
2018-10-13 20:15:47 +0000

    Added Consul API client

---
+++ nomadgen/consul_meta.py
@@ -0,0 +1,32 @@
+from nomadgen.struct_parser import StructParser
+CONSUL_VERSION='1.2.2'
+
+if __name__ == "__main__":
+    StructParser(
+        version='v' + CONSUL_VERSION,
+        product='consul',
+        override_field={
+            'ServiceDefinitionConnectProxy':{'Config':'string'},
+            'ServiceNode':{
+                'ServiceKind':'string',
+            },
+        },
+        tr_types={
+            'api.ReadableDuration': 'string',
+            'types.NodeID': 'string',
+            'types.CheckID': 'string',
+        },
+        file_paths=[
+            '/agent/structs/structs.go',
+            '/agent/structs/service_definition.go',
+        ],
+        start_keys=[
+            'QueryMeta',
+            'IndexedNodes',
+            'IndexedServices',
+            'IndexedServiceNodes',
+            'IndexedNodeServices',
+            'IndexedHealthChecks',
+            'IndexedCheckServiceNodes',
+            'IndexedNodeDump',
+        ]).print_all()
